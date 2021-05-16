7d439fe789747856b9943bd52772491815fb3a65
Yuriy <kireev_juriyy@rambler.ru>
2021-05-16 13:22:32 +0800

    change the cod in task_2_1.py

--- task_2_1.py
+++ task_2_1.py
@@ -1,6 +1,15 @@
 number = 15
+number_2 = 2
+number_3 = 3
 
-print(type(number*3))
-print(type(number/3))
-print(type(number//2))
-print(type(number**2))
+#Два варианта выяснения типа объеката
+#В первом случае выводится тип объекта
+print(type(number*number_3))
+print(type(number/number_3))
+print(type(number//number_2))
+print(type(number**number_2))
+#Во втором случае выводистя True если тип указан верно
+print('Тип данных целочисленный: ',isinstance(number*number_3, int))
+print('Тип данных дробный: ',isinstance(number/number_3, float))
+print('Тип данных целочисленный: ',isinstance(number//number_2, int))
+print('Тип данных целочисленный: ',isinstance(number**number_2, int))
