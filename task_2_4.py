d0158c894b383ace0bbed8ab540fad0e242e2b40
Yuriy <kireev_juriyy@rambler.ru>
2021-05-17 18:15:21 +0800

    change the cod

--- task_2_4.py
+++ task_2_4.py
@@ -1,10 +1,16 @@
-ls = [57.8, 46.54, 97, 101.5, 15.3, 77.847, 94.02, 50, 2]
+ls = [57.8, 46.54, 97, 101.5, 15.3, 77.84, 94.02, 50.4, 2]
+
 string = ''
 for elem in ls:
 
-    if int(elem//1) < 10:
-        string += f'0{elem:.2f} '
-    else:
-        string += f'{elem:.2f} '
+    price = str(elem).split('.')
+    if len(price) == 1:
+        price.append('00')
+    string += f'{price[0]} руб {price[1]} коп '
 print(string)
+
+
+
+
+
 
