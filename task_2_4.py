d6a2460bf22469e5fcf32d839400a0fdb9ad1825
Yuriy <kireev_juriyy@rambler.ru>
2021-05-17 23:41:23 +0800

    solved the task *

--- task_2_4.py
+++ task_2_4.py
@@ -1,13 +1,35 @@
-ls = [57.8, 46.54, 97, 101.5, 15.3, 77.84, 94.02, 50.4, 2]
+list_price = [57.8, 46.54, 97, 101.5, 15.3, 77.84, 94.2, 50.4, 5.00, 2]
+revers_list_price = [57.8, 46.54, 97, 101.5, 15.3, 77.84, 94.2, 50.4, 5.00, 2]
 
 string = ''
-for elem in ls:
+
+for elem in sorted(list_price):
 
     price = str(elem).split('.')
+
     if len(price) == 1:
         price.append('00')
-    string += f'{price[0]} руб {price[1]} коп '
+    elif price[1] == '0':
+        price[1] = f'0{price[1]}'
+
+    if int(price[0]) // 10 == 0:
+        price[0] = f'0{price[0]}'
+    elif int(price[1]) // 10 == 0 and price[1] != '00':
+        price[1] = f'0{price[1]}'
+
+    if elem != list_price[-1]:
+        string += f'{price[0]} руб {price[1]} коп, '
+    else:
+        string += f'{price[0]} руб {price[1]} коп.'
+
 print(string)
+
+print(list_price)
+
+
+
+
+
 
 
 
