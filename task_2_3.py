9167ab6a78bd3609301d0beb368b1aa82a6b7f8b
Yuriy <kireev_juriyy@rambler.ru>
2021-05-18 12:47:15 +0800

    change the cod in task

--- task_2_3.py
+++ task_2_3.py
@@ -1,6 +1,7 @@
 input_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
 
 for employee_name in input_list:
+
      employee_name = employee_name.lower()
      employee_name = employee_name.split(sep = ' ')
      print (f'Привет, {employee_name[-1].title()}!')
